6fdd52149164c6432d2a4fe313a3c2aad0a51b3d
Jonathan Caballero <jonathancaballero@uninorte.edu.co>
2022-04-20 09:37:53 -0500

    dinamyc colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,21 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
+import colors from 'vuetify/lib/util/colors'
+
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.deepPurple.base, // #E53935
+        secondary: colors.red.darken1, // #FFCDD2
+        accent: colors.indigo.base, // #3F51B5
+      },
+      dark: {
+        primary: colors.blue.darken3,
+      }
+    },
+  },
 });
